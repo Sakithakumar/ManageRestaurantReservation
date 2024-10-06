e7aad2c7eb93e8447271dcf34b59424b4dc518e1
SAKITHA S <sakithakumar04@gmail.com>
2024-10-06 22:09:44 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,38 @@
+<script>
+    const reservations = [];
+
+    function addReservation() {
+        const name = document.getElementById('name').value.trim();
+        const guests = document.getElementById('guests').value.trim();
+        const time = document.getElementById('time').value;
+
+        if (name && guests && time) {
+            const reservation = { name, guests, time };
+            reservations.push(reservation);
+            
+            displayReservations();
+            
+            // Clear input fields
+            document.getElementById('name').value = '';
+            document.getElementById('guests').value = '';
+            document.getElementById('time').value = '';
+        } else {
+          alert("Please fill in all fields.");
+        }
+    }
+
+    function displayReservations() {
+        const list = document.getElementById('reservations-list');
+        list.innerHTML = '';
+        
+        reservations.forEach((res, index) => {
+            list.innerHTML += `<li>${res.name} - ${res.guests} guests at ${new Date(res.time).toLocaleString()} 
+                <button onclick="cancelReservation(${index})">Cancel</button></li>`;
+        });
+    }
+
+    function cancelReservation(index) {
+        reservations.splice(index, 1);
+        displayReservations();
+    }
+</script>
